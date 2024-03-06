3009bfa6ec090e04cd6029d254b3a1d4fef728bf
Milutin25 <stefanovicfamily@Stefanovics-iMac.local>
2024-03-06 12:20:28 +0100

    main

---
+++ main.js
@@ -0,0 +1,8 @@
+let age = 42; //number
+let bigNumber = 9999999999999999n; //bigint
+let firstName = "Milutin"; //string
+var x = 5==6; //boolean
+var a = null; //null
+var b; //undefined
+const ID = symbol; //symbol
+let car ={make:"Toyota", model:"Highlender", year:2021} //object
