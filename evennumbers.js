dc57d11257b130f097a17b03c1871f965bdbc9ed
Milutin25 <stefanovicfamily@Stefanovics-iMac.local>
2024-03-13 17:40:40 +0100

    initial commit

---
+++ evennumbers.js
@@ -0,0 +1,9 @@
+let n = 30;
+let sum = 0;
+for (let i = 1; i <= n; i ++){
+    if ( i % 2 ===0){
+        sum = sum + 1;       
+    } 
+    }
+    console.log(sum);
+
---
+++ factorial.js
@@ -0,0 +1,6 @@
+const n = 5;
+let sum = 1; // 1 is here becuase if we multiply any number with 0 is always 0?
+for ( let i=1; i <=n; i++){
+    sum = sum * i;
+}
+console.log(sum);
